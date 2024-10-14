13249b0f27ef3ec6b8d3f1e02ef43f2c1aa51490
Facucoyne <hunter.bl12@yahoo.com>
2024-10-14 16:16:29 -0300

    Primer Commit

---
+++ js/main.js
@@ -0,0 +1,36 @@
+
+    function comprar(){
+        const swalWithBootstrapButtons = Swal.mixin({
+            customClass: {
+            confirmButton: "btn btn-success",
+            cancelButton: "btn btn-danger"
+            },
+            buttonsStyling: false
+        });
+        swalWithBootstrapButtons.fire({
+            title: "Deseas Comprar El Producto?",
+            text: "Escoge una opcion!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonText: "Comprar!",
+            cancelButtonText: "Cancelar!",
+            reverseButtons: true
+        }).then((result) => {
+            if (result.isConfirmed) {
+            swalWithBootstrapButtons.fire({
+                title: "Compra Realizada!",
+                text: "Gracias por comprar nuestro producto.",
+                icon: "success"
+            });
+            } else if (
+            /* Read more about handling dismissals below */
+            result.dismiss === Swal.DismissReason.cancel
+            ) {
+            swalWithBootstrapButtons.fire({
+                title: "Producto Eliminado",
+                text: "Vuelve Pronto",
+                icon: "error"
+            });
+            }
+        });
+    }
